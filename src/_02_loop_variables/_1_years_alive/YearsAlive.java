ca333472c5ac5a928185fcfdf195e4144dfbfb5e
league <league@imac-20.attlocal.net>
2023-11-29 16:50:30 -0800

    Today I FINALLY finished Own Adventure and Years Alive.

---
+++ src/_02_loop_variables/_1_years_alive/YearsAlive.java
@@ -0,0 +1,11 @@
+package _02_loop_variables._1_years_alive;
+
+public class YearsAlive {
+
+
+public static void main(String[] args) {
+for(int i=2013; i<2024; i++) {
+System.out.println(i);	
+}
+}
+}
