8571500f3458b4b036bada9ebb79d987a796b5b0
league <matthewseul>
2024-02-17 11:19:40 -0800

    Today I finished Module 3 (Birthday Reminder and Dizzy Robot).

--- src/_99_extra/BirthdayReminder.java
+++ src/_99_extra/BirthdayReminder.java
@@ -8,21 +8,34 @@ public class BirthdayReminder {
 	public static void main(String[] args) {
 	
 		// 1. correct the birthdays for your family below
-		String momsBirthday = "January 24th";
-		String dadsBirthday = "June 9th";
-		String myBirthday = "April 16th";
+		String momsBirthday = "November 15th";
+		String dadsBirthday = "December 31st";
+		String myBirthday = "April 11th";
 
 		// 2. Find out which birthday the user wants and and store their response in a variable
-	
+		String input = JOptionPane.showInputDialog("Which birthday would you like?");
 		// 3. Print out what the user typed
-		
+		System.out.println(input);
 		// 4. if user asked for "mom"
 			//print mom's birthday
+		if(input.equals("mom")) {
+		System.out.println("November 15th");	
+		}
 		// 5. if user asked for "dad"
 			// print dad's birthday
+		if(input.equals("dad")) {
+		System.out.println("December 31st");	
+		}
 		// 6. if user asked for your name
 			// print myBirthday
-		//7. otherwise print "Sorry, i don't remember that person's birthday!"
-
+		if(input.equals("Your birthday")) {
+		System.out.println("April 11th");	
+		}
+		//7. otherwise print "Sorry, I don't remember that person's birthday!"
+		if(input.equals("Mine")) {
+		System.out.println("Sorry, I don't remember that person's birthday!");	
+		}
+		
+		
 	} 
 }
--- src/_99_extra/DizzyRobot.java
+++ src/_99_extra/DizzyRobot.java
@@ -1,16 +1,22 @@
 
 package _99_extra;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class DizzyRobot {
 	static Robot robot = new Robot();
 	public static void main(String[] args) {
 		
-		// 1. Use the dance method below to make the robot spin.		
+		// 1. Use the dance method below to make the robot spin.
+		
 		// 2. Ask the user how dizzy you want the robot from 1-10, then spin that number of times.
-	
-
+		String input = JOptionPane.showInputDialog("How dizzy would you like the robot to be from 1-10?");
+		int i = Integer.parseInt(input);
+		dance(i);
+			
+		
 	}
 
 	/*****************   Use this method, DON'T CHANGE THE CODE BELOW  **************/
@@ -21,4 +27,3 @@ public class DizzyRobot {
 		}
 	}
 }
-
